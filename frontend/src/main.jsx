6a8bf4efb7b2fa6ca98ace83dfed4f13eca4ad2c
Gustavo <gumartinsr120@gmail.com>
2025-04-30 09:33:53 -0300

    feat: add cadastro route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "@/styles/globals.css"
 
 import { Login } from "@/pages/Login"
+import { Cadastro } from "@/pages/cadastro"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />
+  },
+  {
+    path: "/cadastro",
+    element: <Cadastro />
   }
 ])
 
