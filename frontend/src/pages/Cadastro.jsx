5ec413ed03ccc585bf74676d76bd4f915ea40c4e
Mariana Rolim de Oliveira <marianarolimdeo@gmail.com>
2025-08-22 23:21:01 -0300

    adicionando tela de cadastro

--- frontend/src/pages/Cadastro.jsx
+++ frontend/src/pages/Cadastro.jsx
@@ -1,69 +1,52 @@
+import { Link } from "react-router-dom";
+
 import banner from "@/img/banner.png";
 import logo from "@/img/logo.png";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+
 export function Cadastro() {
   return (
-    <main className="container">
-      <img className="banner" src={banner} />
+    <main className="flex">
+      <img src={banner} className="h-screen" />
 
-      <div className="login-content cadastro-content">
-        <img className="logo" src={logo} />
-
-        <div className="titles-content">
-          <h1>Começe aqui</h1>
-          <h2>Crie uma nova conta</h2>
+      <div className="flex flex-col justify-center gap-10 mt-20 max-w-xl !mx-auto w-full">
+        <div className="flex flex-col gap-4">
+          <img src={logo} className="w-24 !mb-6" />
+          <h1 className="text-6xl font-semibold">Bem vindo!</h1>
+          <p>Faça o login em sua conta</p>
         </div>
 
-        <form className="login-forms">
-          <div className="login-email">
-            <label id="email">Nome</label>
-            <input
-              className="input-login"
-              type="email"
-              id="email"
-              placeholder="Bruno Reginaldo dos Santos"
-            />
+        <form className="flex flex-col gap-8">
+          <div className="flex flex-col gap-3">
+            <Label for="email">Email</Label>
+            <Input type="email" id="email" placeholder="seuemail@gmail.com" />
           </div>
+          <div className="flex flex-col gap-3">
+            <Label for="senha">Senha</Label>
 
-          <div className="login-email">
-            <label id="email">Email</label>
-            <input
-              className="input-login"
-              type="email"
-              id="email"
-              placeholder="seuemail@gmail.com"
-            />
+            <Input type="password" id="senha" placeholder="**********" />
           </div>
-
-          <div className="login-senha">
-            <label>Senha</label>
-            <input
-              className="input-login"
-              type="password"
-              id="senha"
-              placeholder="**********"
-            />
+          <div className="flex item-center gap-2">
+            <Checkbox id="termos" />
+            <Label for="termos">
+              Aceito os{" "}
+              <span className="text-primary underline underline-offset-4 decoration-primary">
+                termos de uso
+              </span>{" "}
+            </Label>
           </div>
+          <Button size="xl">Fazer Login</Button>
 
-          <label class="checkbox-container">
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-            Aceito os termos
-          </label>
-
-          <button className="login-button">Registrar-se</button>
-
-          <div className="login-obs">
+          <div className="flex items-center justify-center">
             <p>
-              Já tem uma conta?{" "}
-              <span className="login-cadastro">Fazer Login</span>
-            </p>
-          </div>
-
-          <div className="obs-termos">
-            <p>
-              Continuando, você aceitara nossos{" "}
-              <span>Termos de serviços e Politica de privacidade</span>
+              Não tem uma conta?{" "}
+              <span className="underline underline-offset-4 decoration-neutral-400">
+                Cadastre-se agora
+              </span>
             </p>
           </div>
         </form>
