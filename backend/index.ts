28b5cb17d407d86d8a629bcfcce2465ca606c233
Julio Francisco Bernardino <juliofranciscobernardino@gmail.com>
2026-05-01 12:30:30 -0300

    chore: configure server port and host via environment variables

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,18 @@ import "dotenv/config";
 import app from "./src/app.ts";
 import { createProxy } from "./src/middleware/proxy.ts";
 
-const PORT = Number(process.env.PORT ?? 3002);
-
-const server = createProxy(app);
-
-server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
+if (process.env.PORT == "3002") {
+  const PORT = Number(3002);
+  const server = createProxy(app);
+  server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+  
+} else {
+  const PORT = Number(process.env.PORT ?? 8100);
+  const HOST = process.env.IP ?? "::";
+  const server = createProxy(app);
+  server.listen(PORT, HOST, () => {
+    console.log(`Servidor rodando em http://[${HOST}]:${PORT}`);
+  });
+}
